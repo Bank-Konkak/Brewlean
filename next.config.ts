fbbfc60c9553097ecd64eca7217af7380b7a3cca
joijammaw-alt <joijammaw@gmail.com>
2026-05-15 16:30:34 +0700

    แก้จ้าาา

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"; 
 
 const nextConfig: NextConfig = {
-  basePath: "/brewlean",
+  basePath: "/brewlean", 
   /* config options here */
 };
 
-export default nextConfig;
+export default nextConfig; 
